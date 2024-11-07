04c00ff923f8350577f3aeb6c766afad4b81a668
BM1091 <afif@ppks.edu.my>
2024-11-07 17:35:06 +0800

    new component endpoint

--- obj/Debug/net8.0/ApiGateWay.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiGateWay.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateWay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17b21841e384f0e3f510b40001d8f226b7118725")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dccf913ed4703d2cc0a76156be37d089f8e295d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateWay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateWay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
